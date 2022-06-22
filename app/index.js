0a9014af0e39c372034c45eafa47221f36ae7e85
senaaputra <senaaputra@gmail.com>
2022-06-22 14:21:40 +0700

    [Sena] create register, login, logout, middleware, and added some dependencies

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const router = require("./routes/router")
 const app = express();
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+
+dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
 
 module.exports = router.apply(app);
